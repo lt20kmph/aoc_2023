758e92e2579cb8120e44aa41b8bb9f14ecef02bf
Oli <lostoli@gmail.com>
2023-12-04 21:47:30 +0000

    day 4

--- 2023/src/day_4.jl
+++ 2023/src/day_4.jl
@@ -2,12 +2,56 @@
 
 input = readlines("2023/data/day_4.txt")
 
-function part_1(input)
-    nothing
+function parse_line(line::String)::Int
+  parts = split(line, "|")
+  winning_numbers_part = split(parts[1], ":")[2]
+  my_numbers_part = parts[2]
+  winning_numbers = filter(x -> length(x) > 0, split(winning_numbers_part, " "))
+  my_numbers = filter(x -> length(x) > 0, split(my_numbers_part, " "))
+
+  winning_numbers = parse.(Int, winning_numbers)
+  my_numbers = parse.(Int, my_numbers)
+
+  my_winning_numbers = filter(x -> x ∈ winning_numbers, my_numbers)
+
+  length(my_winning_numbers)
+
+end
+
+function get_score(length::Int)::Int
+  if length == 0
+    return 0
+  end
+
+  2^(length - 1)
+end
+
+function part_1(input::Vector{String})::Int
+  scoreMap = Dict{Int,Int}()
+  for (i, line) in enumerate(input)
+    scoreMap[i] = get_score(parse_line(line))
+  end
+  return sum(values(scoreMap))
 end
 @info part_1(input)
 
-function part_2(input)
-    nothing
+function part_2(input::Vector{String})::Int
+  lengthMap = Dict{Int,Int}()
+  freqMap = Dict{Int,Int}()
+
+  for (i, line) in enumerate(input)
+    lengthMap[i] = parse_line(line)
+    freqMap[i] = 1
+  end
+
+  for i in eachindex(input)
+    length = lengthMap[i]
+    for j in 1:length
+      freqMap[i+j] += freqMap[i]
+    end
+  end
+
+  sum(values(freqMap))
+
 end
 @info part_2(input)
